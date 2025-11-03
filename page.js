ab3c48a8855d0fd482da5b14d11a55e76d9d82ba
Rammohan791 <rammohansingh.cse23@kgikanpur.in>
2025-11-04 01:14:49 +0530

    added index.html

---
+++ page.js
@@ -0,0 +1,11 @@
+	let bntclick=document.getElementById("button");
+    function btn(){
+    window.location.href="kandya.html";}
+	
+let quizzbutton=document.getElementById("quizzebutton");
+function quizzbtn(){
+window.location.href="quizzpage.html";}
+
+let quizzespage=document.getElementById("quizzesopenbtn");
+function quizzpage(){
+window.location.href="quizzes.html";}
